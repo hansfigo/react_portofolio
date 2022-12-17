120d04928072547fe69a5248c7eb6224a7073602
Hans Figo <hambasahaya0303@gmail.com>
2022-12-17 15:01:55 +0700

    commit\

--- pages/index.js
+++ pages/index.js
@@ -3,56 +3,68 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
 import { BsGithub } from 'react-icons/bs';
 import { BsDribbble } from 'react-icons/bs';
+import { useState } from 'react';
 
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <>
+    <div className={darkMode ? "dark" : " "}>
       <Head>
         <title>Hans Figo Portofolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-white px-10'>
+      <main className='bg-white px-10 transition duration-500 ease-in-out dark:bg-darkBlueBG dark:text-gray-200'>
         <section>
-          <div className='py-6 px-10 border-b w-full z-20 top-0 left-0 fixed bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 text-center border-gray-200'>
+          <div className='py-5 px-10 border-b w-full z-20 top-0 left-0 fixed dark:bg-darkBlueBG dark:bg-opacity-30 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 text-center border-gray-200'>
             <nav className=' flex justify-between '>
-              <h1 className='text-xl font-semibold font-burtons justify-center'>FigoMager</h1>
+              <h1 className='text-xl font-semibold font-burtons justify-center'>HNZCLZ</h1>
               <ul className='flex items-center'>
-                <li className='text-xl cursor-pointer'>
+                <li onClick={() => setDarkMode(!darkMode)} className='text-xl cursor-pointer'>
                   <BsFillMoonStarsFill />
                 </li>
                 <li>
-                  <a className='font-semibold rounded-md ml-8 px-4 py-2 bg-gradient-to-r from-blue-500 to-cyan-500 text-white text-lg' href="#">Resume</a>
+
+                  <div className="HAMBURGER-ICON space-y-2 ml-8">
+                    <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
+                    <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
+                    <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
+                  </div>
+
                 </li>
+                {/* <li>
+                  <a className='font-semibold rounded-md ml-8 px-4 py-2 bg-gradient-to-r from-blue-500 to-cyan-500 text-white text-lg' href="#">Resume</a>
+                </li> */}
               </ul>
             </nav>
           </div>
-          <div className='text-center py-28'>
+          <div className='text-center py-28 '>
             <h2 className='font-bold text-4xl text-blue-500'>Claudio Hans Figo</h2>
             <h3 className='font-bold text-xl py-4 '>Developer and Wibu Stress</h3>
-            <p className='leading-8 text-gray-500'>
+            <p className='leading-8 text-gray-500 dark:text-gray-400'>
               Hello i'm Figo, I'm Madoka Magica SuperFan,
               Interested in Software Engineering and System Security.
               Currently Studying in Amikom University of Yogyakarta.
             </p>
             <nav className='py-10'>
-              <ul className='flex justify-center text-4xl gap-12 text-gray-500 '>
-                <li className='hover:text-darkBlue'><BsLinkedin /></li>
-                <li className='hover:text-darkBlue'><BsGithub /></li>
-                <li className='hover:text-darkBlue'><BsDribbble /></li>
+              <ul className='flex justify-center text-4xl gap-12 text-gray-500 dark:text-gray-400'>
+                <li className='hover:text-darkBlue dark:hover:text-white'><BsLinkedin /></li>
+                <li className='hover:text-darkBlue dark:hover:text-white'><BsGithub /></li>
+                <li className='hover:text-darkBlue dark:hover:text-white'><BsDribbble /></li>
               </ul>
             </nav>
           </div>
           <div className="flex flex-wrap justify-center">
             <div className="w-3/4 sm:w-4/12 px-4 py-8">
-              <img src="ryo_avatar.jpg" class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+              <img src="ryo_avatar.jpg" class=" shadow-avatar dark:shadow-teal-800 rounded-full max-w-full h-auto align-middle border-none" />
             </div>
           </div>
           <div className='py-10'>
-            <h3 className='font-bold text-xl py-4 '>About me</h3>
-            <p className='leading-8 text-gray-500'>
+            <h3 className='text-blue-500 font-bold text-xl dark:drop-shadow-md dark:shadow-blue-600'>About me</h3>
+            <h2 className='font-bold text-2xl py-2'>Let's be a Friend !!</h2>
+            <p className='leading-8 text-gray-500 py-2  dark:text-gray-400'>
               Graduated from Informatic engineering of SMKN 2 Depok,
               Now Studying in AMIKOM University of Yogyakarta.
               Interested in Software Engineering and Cyber Security.
@@ -62,6 +74,6 @@ export default function Home() {
         </section>
 
       </main>
-    </>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'avatar': '0 0px 40px -px rgba(0, 0, 0, 0.2)',
+      },
       colors:{
         'darkBlue': '#323A50',
+        'darkBlueBG' : '#0B1121',
       },
       fontFamily:{
         burtons:"burtons",
