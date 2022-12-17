c3c445cd1367ed6a349c92400bf85eef601e431a
Hans Figo <hambasahaya0303@gmail.com>
2022-12-17 16:10:34 +0700

    modified:   pages/index.js 	modified:   tailwind.config.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import { useState } from 'react';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <div className={darkMode ? "dark" : " "}>
       <Head>
@@ -22,17 +24,46 @@ export default function Home() {
             <nav className=' flex justify-between '>
               <h1 className='text-xl font-semibold font-burtons justify-center'>HNZCLZ</h1>
               <ul className='flex items-center'>
-                <li onClick={() => setDarkMode(!darkMode)} className='text-xl cursor-pointer'>
+                <li onClick={() => setDarkMode(!darkMode)} className={!isNavOpen ? 'text-xl cursor-pointer ': 'hidden'}>
                   <BsFillMoonStarsFill />
                 </li>
                 <li>
 
-                  <div className="HAMBURGER-ICON space-y-2 ml-8">
+                  <div className={!isNavOpen ? 'HAMBURGER-ICON lg:hidden space-y-2 ml-8' : 'hidden'} onClick={() => setIsNavOpen((prev) => !prev)}>
                     <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
                     <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
                     <span className="block h-0.5 w-6 animate-pulse bg-gray-200"></span>
                   </div>
-
+                  <div className={isNavOpen ? "flex justify-evenly flex-col py-4 items-stretch" : "hidden transition duration-500 ease-in-out"}>
+                    <div
+                      className="CROSS-ICON absolute top-0 right-0 px-8 py-4 cursor-pointer"
+                      onClick={() => setIsNavOpen(false)}
+                    >
+                      <svg
+                        className="h-8 w-8 text-gray-600"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <line x1="18" y1="6" x2="6" y2="18" />
+                        <line x1="6" y1="6" x2="18" y2="18" />
+                      </svg>
+                    </div>
+                    <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-center  bg-red-100 py-10">
+                      <li className="border-b border-gray-400 my-8 uppercase">
+                        <a href="/about">About</a>
+                      </li>
+                      <li className="border-b border-gray-400 my-8 uppercase">
+                        <a href="/portfolio">Portfolio</a>
+                      </li>
+                      <li className="border-b border-gray-400 my-8 uppercase">
+                        <a href="/contact">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
                 </li>
                 {/* <li>
                   <a className='font-semibold rounded-md ml-8 px-4 py-2 bg-gradient-to-r from-blue-500 to-cyan-500 text-white text-lg' href="#">Resume</a>
@@ -40,6 +71,25 @@ export default function Home() {
               </ul>
             </nav>
           </div>
+          <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 50vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
           <div className='text-center py-28 '>
             <h2 className='font-bold text-4xl text-blue-500'>Claudio Hans Figo</h2>
             <h3 className='font-bold text-xl py-4 '>Developer and Wibu Stress</h3>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
+  isNavOpen : 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
